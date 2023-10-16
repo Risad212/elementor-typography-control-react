da8cadea5599294d63d77d20f013c155965d68bc
Risad212 <hafez.risad@gmail.com>
2023-10-16 11:04:05 +0600

    done

--- src/components/typography/Typography.jsx
+++ src/components/typography/Typography.jsx
@@ -9,24 +9,35 @@ import './typography.css';
 function typography() {
     return (
         <>
-            <div className="typography-control">
-                <div className="typography-header">
-                    <span className='title'>Typography</span>
-                    <div className="control-tools">
-                        <button className='control-undo'><i class="fa-solid fa-rotate-left"></i></button>
-                        <button class="e-control-tool"><i class="fa-solid fa-plus"></i></button>
+            <div className="main-control">
+                <div className="main-control-wrap">
+                    <div className='main-control-title'>
+                        <span>Typography</span>
+                    </div>
+                    <div className="main-control-icon">
+                        <span className='icon'><i class="fa-solid fa-globe"></i></span>
+                        <span className="icon"><i class="fa-solid fa-pen-to-square"></i></span>
                     </div>
                 </div>
-                {/*------- font control ----------*/}
-                <Fontfamily />
-               {/*------- font width control ----------*/}
-                <Fontweight />
-                {/*------- transform control ----------*/}
-                <Transform />
-                {/*------- font style control ----------*/}
-                <Fontstyle />
-                {/*------- text decoration control ----------*/}
-                 <Textdecoration />
+                <div className="typography-control">
+                    <div className="typography-header">
+                        <span className='title'>Typography</span>
+                        <div className="control-tools">
+                            <button className='control-undo'><i class="fa-solid fa-rotate-left"></i></button>
+                            <button class="e-control-tool"><i class="fa-solid fa-plus"></i></button>
+                        </div>
+                    </div>
+                    {/*------- font control ----------*/}
+                    <Fontfamily />
+                    {/*------- font width control ----------*/}
+                    <Fontweight />
+                    {/*------- transform control ----------*/}
+                    <Transform />
+                    {/*------- font style control ----------*/}
+                    <Fontstyle />
+                    {/*------- text decoration control ----------*/}
+                    <Textdecoration />
+                </div>
             </div>
         </>
     )
